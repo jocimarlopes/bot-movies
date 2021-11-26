9d7c0b7d2fd511f8e6b75dd5857d3161d55d8577
Jocimar Lopes <lopes.jocimar@gmail.com>
2021-11-26 04:07:28 -0300

    fix/added_transmission_cli_commands

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 import time
-import random
 import platform
+import subprocess
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 i = 0
 path = 'geckodriver'
@@ -17,8 +21,9 @@ class Bot:
     def init(args):
 
         movie_search = args.m + ' dublado download hd torrent'
-
-        driver = webdriver.Firefox(executable_path=path)
+        options = Options()
+        options.headless = True
+        driver = webdriver.Firefox(options=options, executable_path=path)
         driver.maximize_window()
         driver.get('https://google.com')
 
@@ -26,29 +31,29 @@ class Bot:
 
         element = driver.find_element_by_name('q')
         element.clear()
+        print('Searching movie..')
         element.send_keys(movie_search)
         element.send_keys(Keys.ENTER)
         time.sleep(3)
+        print('Start search in Google List')
         atual_url = driver.current_url
         driver.get(atual_url)
-        Bot.goSearch('\nFilme: ' + args.m, driver)
+        Bot.goSearch('\nMovie: ' + args.m, driver)
 
     def goSearch(data, driver):
-        time.sleep(2.1)
-        print('\n\nWait.. ')
-        time.sleep(1)
         print('Searching Torrent in Google Search... ', data)
+        time.sleep(2.5)
         Bot.findInSearch('Torrent', 'Download', driver)
 
     def findInSearch(var, pos, driver):
         returnURL = driver.current_url
-        time.sleep(1)
-        print('Find the Movie, wait..')
+        time.sleep(0.5)
         array = driver.find_elements_by_class_name("g")
         list = len(array)
         global i
         if i < list:
             element = driver.find_elements_by_class_name("yuRUbf")
+            print((i + 1), 'º Item in List')
             element[i].click()
             time.sleep(4)
             i = i + 1
@@ -59,27 +64,25 @@ class Bot:
     def verifyLinkInPage(driver, url, p):
         page_source = driver.page_source
         if 'magnet:' in page_source:
-            print('Torrent: TRUE')
-            time.sleep(1)
-            print('Go to Link Download\n')
+            print('Torrent Found: TRUE')
+            time.sleep(0.5)
+            print('Getting Magnet Link from Source\n')
             Bot.goPageDownload(driver)
         else:
-            print('Torrent: FALSE')
+            print('Torrent Found: FALSE')
             time.sleep(1)
-            print('Return to Google Search')
+            print('Return to Google Search List')
             driver.get(url)
-            time.sleep(2)
-            print('and Research in List\n\n')
+            time.sleep(2.5)
+            print('Research in List..\n\n')
             Bot.goSearch(p, driver)
 
     def goPageDownload(driver):
         current_url = driver.current_url
         source = "view-source:" + current_url
-
         driver.get(source)
-        time.sleep(2)
-
+        time.sleep(3)
         element = driver.find_element_by_tag_name("a[href*='magnet:']")
         time.sleep(1)
-        print('\n')
-        print(element.get_attribute('href'))
+        print('\nInitializing Transmission-cli')
+        subprocess.run(["transmission-cli", element.get_attribute('href')])
